2e2337de76ab4dc7b8426d555fba5c67bd58b9c5
Aaron Levin <vilevin@gmail.com>
2015-07-06 10:29:13 -0400

    Partial commit to upload work

---
+++ src/Network/HTTP/Client/Free/Util.hs
@@ -0,0 +1,17 @@
+{-| Utilities for working with networking types
+-}
+
+{-# LANGUAGE BangPatterns #-}
+
+module Network.HTTP.Client.Free.Util (
+    setMethod
+) where
+
+import Network.HTTP.Client (Request(method))
+import Network.HTTP.Types.Method (StdMethod, renderStdMethod)
+
+-- | set the method of a request, overriding the previous method.
+setMethod :: StdMethod -> Request -> Request
+setMethod m req = let !nMethod = renderStdMethod m in req { method = nMethod }
+
+
